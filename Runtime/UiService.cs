9d7c401cbf1078580cc1d98faee34957741eaec0
Miguel Tomas <miguel@gram.gs>
2020-01-07 10:41:10 +0000

    Fixed relationship with other packages

--- Runtime/UiService.cs
+++ Runtime/UiService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using GameLovers.LoaderExtension;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
